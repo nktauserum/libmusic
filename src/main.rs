d364d07afccc274dc161403a6bec69202d0f0d44
nktauserum <auserum@proton.me>
2025-09-22 18:52:03 +0300

    impl: axum setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:6432").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> &'static str {
+    "Hello, World!"
 }
